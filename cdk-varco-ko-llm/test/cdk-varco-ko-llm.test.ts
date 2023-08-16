fb67bc226cfb8296c7be13574f19254cfb9e1400
ksdyb.park <ksdyb@amazon.com>
2023-08-16 15:27:23 +0900

    update

---
+++ cdk-varco-ko-llm/test/cdk-varco-ko-llm.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkVarcoKoLlm from '../lib/cdk-varco-ko-llm-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-varco-ko-llm-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkVarcoKoLlm.CdkVarcoKoLlmStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
